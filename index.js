ddfe6adf498042388dc3f971ae93196ee8cd0e31
Teacher <teacher@1P-105233173.local>
2020-12-01 18:33:31 -0500

    added code to index

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Add routes, both API and view
 app.use(routes);
