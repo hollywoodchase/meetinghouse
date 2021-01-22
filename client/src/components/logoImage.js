7cec81f62a3a5c46e9ac74d76e811a2a67b7fd6f
Daniel Calise <dannyjcalise@gmail.com>
2021-01-22 14:45:52 -0500

    update logo

--- client/src/components/logoImage.js
+++ client/src/components/logoImage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import img from '../images/hayden.jpg'; 
+import img from '../images/logo.jpg'; 
 
 function LogoImage() {
   return <img src={img} id="logo-image" alt="Img" />;
